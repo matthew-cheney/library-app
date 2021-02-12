23d3cbcfa5a53491f24c8a3332739e48d75a67d8
Jordan Hall <jordanhall@Jordans-MacBook-Pro.local>
2021-02-12 10:30:20 -0700

    initial commit

---
+++ front_end/app/src/main/java/com/example/libraryofpeers/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.libraryofpeers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+mkdir
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
