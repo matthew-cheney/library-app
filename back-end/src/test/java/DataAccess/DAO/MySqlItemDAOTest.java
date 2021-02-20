7e09f7a62601f6ff2c8a4b6ad5a537032d56ff71
cwood <cwood@efilecabinet.com>
2021-02-20 15:32:12 -0700

    Finished setting up Maven

--- back-end/src/test/java/DataAccess/DAO/MySqlItemDAOTest.java
+++ back-end/src/test/java/DataAccess/DAO/MySqlItemDAOTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MySqlItemDAOTest {
 
     IItemDAO dao;
