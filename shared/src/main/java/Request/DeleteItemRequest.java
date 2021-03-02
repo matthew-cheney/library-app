db05bcf761160c32561433cd59c8d88c0266bca6
cwood <cwood@efilecabinet.com>
2021-03-02 11:12:26 -0700

    Filled out more requests and responses

--- shared/src/main/java/Request/DeleteItemRequest.java
+++ shared/src/main/java/Request/DeleteItemRequest.java
@@ -1,4 +1,20 @@
 package Request;
 
 public class DeleteItemRequest {
+
+    private String id;
+
+    public DeleteItemRequest() {}
+
+    public DeleteItemRequest(String id) {
+        setId(id);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
