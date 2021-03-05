6bf8db243dbf7a01f28c25ade67c49ca3e2c7528
Matthew Cheney <m.cheney95@outlook.com>
2021-03-04 19:44:22 -0700

    Working register and login

--- front_end/app/src/main/java/com/example/libraryofpeers/net/ServerFacade.java
+++ front_end/app/src/main/java/com/example/libraryofpeers/net/ServerFacade.java
@@ -1,5 +1,9 @@
 package com.example.libraryofpeers.net;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import java.io.IOException;
 
 import Request.LoginRequest;
@@ -8,13 +12,15 @@ import Response.LoginResponse;
 import Response.RegisterResponse;
 
 public class ServerFacade {
-    private static final String SERVER_URL = "";
+    private static final String SERVER_URL = "https://uvxkvq54nl.execute-api.us-east-2.amazonaws.com/beta";
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public LoginResponse login(LoginRequest request, String urlPath) throws IOException {
         ClientCommunicator communicator = new ClientCommunicator(SERVER_URL);
         return communicator.doPost(urlPath, request, null, LoginResponse.class);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public RegisterResponse register(RegisterRequest request, String urlPath) throws IOException {
         ClientCommunicator communicator = new ClientCommunicator(SERVER_URL);
         return communicator.doPost(urlPath, request, null, RegisterResponse.class);
