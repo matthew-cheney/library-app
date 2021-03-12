fbe37774e8866bc16d5b63b51d8b44f9de1b873c
cwood <cwood@efilecabinet.com>
2021-03-12 08:08:46 -0500

    Adjusted date format

--- shared/src/main/java/Config/Constants.java
+++ shared/src/main/java/Config/Constants.java
@@ -5,6 +5,6 @@ import java.text.SimpleDateFormat;
 public class Constants {
 
     public static final int BATCH_SIZE = 10;
-    public static final SimpleDateFormat ITEM_DATE_FORMAT = new SimpleDateFormat("dd-MMM-yyyy");
+    public static final SimpleDateFormat ITEM_DATE_FORMAT = new SimpleDateFormat("mm-dd-yyyy");
     public static final SimpleDateFormat TOKEN_DATE_FORMAT = new SimpleDateFormat("YYYY-MM-DD HH-MM-SS");
 }
