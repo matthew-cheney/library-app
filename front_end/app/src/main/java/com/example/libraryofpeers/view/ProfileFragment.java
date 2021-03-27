496d29a479e663c4e09a5eb46378ab7166ba35f0
Matthew Cheney <m.cheney95@outlook.com>
2021-03-27 11:51:09 -0600

    Temporary fix for going back from profile editor

--- front_end/app/src/main/java/com/example/libraryofpeers/view/ProfileFragment.java
+++ front_end/app/src/main/java/com/example/libraryofpeers/view/ProfileFragment.java
@@ -1,11 +1,13 @@
 package com.example.libraryofpeers.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import Entities.User;
 import Request.EditUserRequest;
 import Response.EditUserResponse;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -50,6 +52,16 @@ public class ProfileFragment extends Fragment implements EditProfileTask.EditPro
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         user = LoginServiceProxy.getInstance().getCurrentUser();
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
+            @Override
+            public void handleOnBackPressed() {
+                Intent mainIntent = new Intent(getContext(), MainActivity.class);
+                startActivity(mainIntent);
+            }
+        };
+        requireActivity().getOnBackPressedDispatcher().addCallback(this, callback);
+
 
     }
 
